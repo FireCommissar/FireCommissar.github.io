1e4645f374f50200a78724016347e314532a51e3
FireCommissar <113386886+FireCommissar@users.noreply.github.com>
2022-10-17 01:04:15 +0300

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,7 @@
+function itog()
+{
+let f1=Number(document.getElementById("field1").value);
+let f2=Number(document.getElementById("field2").value);
+let f3=f1*f2;
+document.getElementById("f3").innerHTML = f3;
+}
