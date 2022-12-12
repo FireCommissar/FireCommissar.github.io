d2165701cd416ff17537114ae0a14d395295dae5
FireCommissar <113386886+FireCommissar@users.noreply.github.com>
2022-12-12 21:52:25 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -3,5 +3,9 @@ function itog()
 let f1=Number(document.getElementById("field1").value);
 let f2=Number(document.getElementById("field2").value);
 let f3=f1*f2;
+if ((f1>0)&&(f2>0))
+{
 document.getElementById("f3").innerHTML = f3;
+}
+else {document.getElementById("f3").innerHTML = "Введите корректные данные (без отрицательных чисел)";}
 }
